e9742755e83969adb830bf737bf8c6425880133a
Miqo1337 <kotanjyan.mikayel@student.rau.am>
2024-01-14 17:15:17 +0400

    Functions are now returning the matches.

--- orb.py
+++ orb.py
@@ -13,15 +13,6 @@ def orb(image1, image2):
     #Image2 turning Gray
     image2_gray = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
 
-    # Display traning image and testing image --> image1/image2
-    fx, plots = plt.subplots(1, 2, figsize=(20,10))
-
-    plots[0].set_title("Training Image")
-    plots[0].imshow(image1)
-
-    plots[1].set_title("Testing Image")
-    plots[1].imshow(image2)
-
     orb = cv2.ORB_create()
 
     train_keypoints, train_descriptor = orb.detectAndCompute(image1_gray, None)
@@ -29,25 +20,6 @@ def orb(image1, image2):
 
     keypoints_without_size = np.copy(image1)
     keypoints_with_size = np.copy(image2)
-
-    cv2.drawKeypoints(image1, train_keypoints, keypoints_without_size, color = (0, 255, 0))
-
-    cv2.drawKeypoints(image1, train_keypoints, keypoints_with_size, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    # Display image with and without keypoints size
-    fx, plots = plt.subplots(1, 2, figsize=(20,10))
-
-    plots[0].set_title("Train keypoints With Size")
-    plots[0].imshow(keypoints_with_size, cmap='gray')
-
-    plots[1].set_title("Train keypoints Without Size")
-    plots[1].imshow(keypoints_without_size, cmap='gray')
-
-    # Print the number of keypoints detected in the training image
-    print("Number of Keypoints Detected In The Training Image: ", len(train_keypoints))
-
-    # Print the number of keypoints detected in the query image
-    print("Number of Keypoints Detected In The Query Image: ", len(test_keypoints))
 
     # Create a Brute Force Matcher object.
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
@@ -58,4 +30,6 @@ def orb(image1, image2):
     # The matches with shorter distance are the ones we want.
     matches = sorted(matches, key = lambda x : x.distance)
 
-    return matches
+    result = cv2.drawMatches(image1, train_keypoints, image2_gray, test_keypoints, matches, image2_gray, flags = 2)
+
+    return result
--- surf.py
+++ surf.py
@@ -12,15 +12,6 @@ def surf(image1, image2):
 
     image2_gray = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
 
-    # Display image1 and image2
-    fx, plots = plt.subplots(1, 2, figsize=(20, 10))
-
-    plots[0].set_title("Image 1")
-    plots[0].imshow(image1)
-
-    plots[1].set_title("Image 2")
-    plots[1].imshow(image2)
-
     # ## Detect keypoints and Create Descriptor
 
     surf = cv2.xfeatures2d.SURF_create(800)
@@ -28,27 +19,7 @@ def surf(image1, image2):
     image1_keypoints, image1_descriptor = surf.detectAndCompute(image1_gray, None)
     image2_keypoints, image2_descriptor = surf.detectAndCompute(image2_gray, None)
 
-    keypoints_without_size = np.copy(image1)
-    keypoints_with_size = np.copy(image1)
-
-    cv2.drawKeypoints(image1, image1_keypoints, keypoints_without_size, color=(0, 255, 0))
-
-    cv2.drawKeypoints(image1, image1_keypoints, keypoints_with_size, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
     # Display image with and without keypoints size
-    fx, plots = plt.subplots(1, 2, figsize=(20, 10))
-
-    plots[0].set_title("Image 1 keypoints With Size")
-    plots[0].imshow(keypoints_with_size, cmap='gray')
-
-    plots[1].set_title("Image 1 keypoints Without Size")
-    plots[1].imshow(keypoints_without_size, cmap='gray')
-
-    # Print the number of keypoints detected in Image 1
-    print("Number of Keypoints Detected In Image 1: ", len(image1_keypoints))
-
-    # Print the number of keypoints detected in Image 2
-    print("Number of Keypoints Detected In The Image 2: ", len(image2_keypoints))
 
     # ## Matching Keypoints
 
@@ -61,4 +32,6 @@ def surf(image1, image2):
     # The matches with shorter distance are the ones we want.
     matches = sorted(matches, key=lambda x: x.distance)
 
-    return matches
+    result = cv2.drawMatches(image1, image1_keypoints, image2_gray, image2_keypoints, matches, image2_gray, flags=2)
+
+    return result
